3441cfabb582f0e47adf08281c15ca1a3aebe488
TARO JINKAWA <jtaroxxx@gmail.com>
2018-04-02 18:51:09 +0900

    Q9まで完了

---
+++ Q1.sql
@@ -0,0 +1,1 @@
+CREATE TABLE item_category (category_id int PRIMARY KEY AUTO_INCREMENT,category_name VARCHAR(256) NOT NULL);
---
+++ Q9.sql
@@ -0,0 +1,1 @@
+SELECT item_category.category_name,SUM(item.item_price) total_price FROM item_category INNER JOIN item ON item.category_id = item_category.category_id GROUP BY item_category.category_id ORDER BY total_price DESC;
